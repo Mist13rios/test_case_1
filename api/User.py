eebf2881ef4a45c6b7382975df9122a696545557
Kirill Timukov <k_timukov@mail.ru>
2019-09-26 17:36:04 +0300

    Заготовка для API

---
+++ api/User.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Session
+from flask_restful import Resource, Api
+
+from app import app
+api = Api(app)
+
+
+class UserApi(Resource):
+
+    def get(self, user_id):
+        return {"hello': 'user_id"}
+
+    def post(self, user_id):
+        return {"waiting for new: 'user_id"}
+
+    def put(self, user_id):
+        return {"let's change something': 'user_id"}
+
+    def delete(self, user_id):
+        return {"deleted user': 'user_id"}
+
+api.add_resource(UserApi, "/user/<string:user_id>")
--- app.py
+++ app.py
@@ -10,6 +10,5 @@ app.secret_key = env_value('SECRET_KEY')
 def hello_world():
     return 'Hello World!'
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000')
